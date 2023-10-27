5f248afd9624fefefd860ff47434ecb98d00741e
Deepanshu Kumar <anshudeep077@outlook.com>
2023-10-27 14:02:01 +0530

    make a add to Cart button

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,6 +4,9 @@ export default function Card(props) {
 
     let options = props.options;
     let priceOption = Object.keys(options)
+    const handleAddToCart = ()=>{
+        
+    }
     return (
         <div>
             {/* <div>
@@ -37,14 +40,14 @@ export default function Card(props) {
                 <div class="card-body">
                     <h5 class="card-title">{props.foodName}</h5>
                     <div className="container w-100">
-                        <select className='m-2 h-100 primary text-light bg-success rounded '>
+                        <select className='m-2 h-100 primary text-primary-emphasis bg-success rounded '>
                             {Array.from(Array(6), (e, i) => {
                                 return (
                                     <option key={i + 1} value={i + 1}>{i + 1}</option>
                                 )
                             })}
                         </select>
-                        <select className='m-2 h-100 text-light  bg-success rounded'>
+                        <select className='m-2 h-100 text-primary-emphasis bg-success rounded'>
                                 {priceOption.map((data)=>{
                                     return <option key={data} value={data}>{data}</option>
                                 })}
@@ -53,6 +56,8 @@ export default function Card(props) {
                         <div className='d-inline h-100 fs-5'>
                             Total Price
                         </div>
+                        <hr />
+                        <button className={`btn btn-success justify-center ms-2`} onClick={handleAddToCart}>Add to Cart</button>
                     </div>
                 </div>
             </div>
