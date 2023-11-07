b2b3a24f6ae0a2d1673ab72d5d37323b2e6dab81
Deepanshu Kumar <anshudeep077@outlook.com>
2023-11-07 15:12:34 +0530

    OrderData FIx

--- backend/Routes/OrderData.js
+++ backend/Routes/OrderData.js
@@ -3,46 +3,72 @@ const router = express.Router();
 
 const Order = require('../models/Orders');
 
-router.post('/orderData', async (req, res)=>{
-    let data = req.body.order_data
-    await data.slice(0, 0, {Order_date: req.body.order_date})
+// router.post('/orderData', async (req, res)=>{
+//     let data = req.body.order_data
+//     await data.slice(0, 0, {Order_date: req.body.order_date})
 
-    let eId = await Order.findOne({'email': req.body.email})
-    console.log(eId)
+//     let eId = await Order.findOne({'email': req.body.email})
+//     console.log(eId)
 
-    if(eId === null){
-        try{
+//     if(eId === null){
+//         try{
+//             await Order.create({
+//                 email: req.body.email,
+//                 order_data: [data]
+//             }).then(()=>{
+//                 res.json({success: true})
+//             })
+//         } catch(error){
+//             console.log(error.message)
+//             // res.send("Server Error", error.message);
+//             res.status("Server Error")
+
+//         }
+//     }
+
+//     else{
+//         try{
+//             await Order.findOneAndUpdate({ email: req.body.email },
+//                 {
+//                     $push: { order_data }}).then(()=>{
+//                         res.json({success: true})
+//                     })
+//         } catch(error){
+//             // res.send("Server Error", error.message)
+//             res.status("Server Error")
+//         }
+//     }
+
+
+// })
+
+router.post('/orderData', async (req, res) => {
+    try {
+        const userEmail = req.body.email;
+        const orderDate = req.body.order_date;
+        const data = req.body.order_data;
+
+        const existingOrder = await Order.findOne({ email: userEmail });
+
+        if (!existingOrder) {
             await Order.create({
-                email: req.body.email,
-                order_data: [data]
-            }).then(()=>{
-                res.json({success: true})
-            })
-        } catch(error){
-            console.log(error.message)
-            // res.send("Server Error", error.message);
-            res.status("Server Error")
-
+                email: userEmail,
+                order_data: [{ Order_date: orderDate, data }],
+            });
+        } else {
+            existingOrder.order_data.push({ Order_date: orderDate, data });
+            await existingOrder.save();
         }
+
+        res.json({ success: true });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: "Server Error" });
     }
+});
 
-    else{
-        try{
-            await Order.findOneAndUpdate({ email: req.body.email },
-                {
-                    $push: { order_data }}).then(()=>{
-                        res.json({success: true})
-                    })
-        } catch(error){
-            // res.send("Server Error", error.message)
-            res.status("Server Error")
-        }
-    }
-
-
-})
 
 module.exports = router;
-// https://youtu.be/iLng4XfiRm8?list=PLI0saxAvhd_OdRWyprSe3Mln37H0u4DAp&t=1246
+// https://youtu.be/iLng4XfiRm8?list=PLI0saxAvhd_OdRWyprSe3Mln37H0u4DAp&t=1281
 
 
--- src/screens/Cart.js
+++ src/screens/Cart.js
@@ -13,24 +13,51 @@ export default function Cart() {
         )
     }
 
-    const handleCheckOut = async()=>{
+    // const handleCheckOut = async()=>{
+    //     let userEmail = localStorage.getItem("userEmail");
+    //     let response = await fetch("http://localhost:5000/api/orderData",{
+    //         method: 'POST',
+    //         headers: {
+    //             'Content-Type': 'application/json'
+    //         },
+    //         body: JSON.stringify({
+    //             order_data: data,
+    //             email: userEmail,
+    //             order_date: new Date().toDateString()
+    //         })
+    //     });
+    //     console.log("Order Response: ", response);
+    //     if(response.status === 200){
+    //         dispatch({type: "DROP"})
+    //     }
+    // }
+
+    const handleCheckOut = async () => {
         let userEmail = localStorage.getItem("userEmail");
-        let response = await fetch("http://localhost:5000/api/orderData",{
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                order_data: data,
-                email: userEmail,
-                order_date: new Date().toDateString()
-            })
-        });
-        console.log("Order Response: ", response);
-        if(response.status === 200){
-            dispatch({type: "DROP"})
+        try {
+            let response = await fetch("http://localhost:5000/api/orderData", {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    order_data: data,
+                    email: userEmail,
+                    order_date: new Date().toDateString()
+                })
+            });
+    
+            if (response.ok) {
+                dispatch({ type: "DROP" });
+            } else {
+                throw new Error(`Request failed with status: ${response.status}`);
+            }
+        } catch (error) {
+            console.error(error);
+            // Handle the error, perhaps by displaying a message to the user
         }
-    }
+    };
+
     let totalPrice = data.reduce((total, food) => total + food.price, 0);
     return (
         <div>
