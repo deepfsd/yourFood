54e817a280f6f853929f2691f46b6be0adfbdb0d
Deepanshu Kumar <anshudeep077@outlook.com>
2023-11-08 13:05:27 +0530

    Some work

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ import '../node_modules/bootstrap/dist/js/bootstrap.bundle';
 import '../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js';
 import Signup from './screens/Signup';
 import { CartProvider } from './components/ContextReducer';
+import MyOrder from './screens/MyOrder.js';
 
 
 function App() {
@@ -25,7 +26,7 @@ function App() {
           <Route exact path="/" element={<Home/>} />
           <Route exact path="/login" element={<Login/>} />
           <Route exact path="/createuser" element={<Signup/>} />
-          
+          <Route exact path="/myOrder" element={<MyOrder/>} />
         </Routes>
     </div>
     </Router>  
